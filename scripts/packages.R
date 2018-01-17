52d8a093cec78620a89e59a3e1dfbc76f6e2effc
bogsnork <christoph.kratz@gmail.com>
2018-01-17 10:20:26 +0000

    create install.packages script
    added script that installs all required packages.

---
+++ scripts/packages.R
@@ -0,0 +1,22 @@
+#Packages used in the scripts
+
+install.packages(c(
+  "caret",
+  "doParallel",
+  "e1071",
+  "kernlab",
+  "parallel",
+  "pROC",
+  "raster",
+  "randomForest",
+  "Rborist",
+  "rgdal", 
+  "rgeos",
+  "sp",
+  "tidyverse"
+))
+
+
+
+
+
